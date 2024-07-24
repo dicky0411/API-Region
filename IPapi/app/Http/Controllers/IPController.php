728828678459ec80fc10a2932b09a88cc7f49e86
dicky0411 <73293265+dicky0411@users.noreply.github.com>
2024-07-24 15:54:11 +0800

    update

---
+++ IPapi/app/Http/Controllers/IPController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Ip2Region;
+use Illuminate\Http\Request;
+
+class IPController extends Controller
+{
+    public function getIpRegion($ip = null)
+    {
+        $ip2region = new Ip2Region();
+        $result = $ip2region->simple($ip);
+        
+
+        // Optionally, you can return the result as a JSON response
+        return response()->json($result);
+    }
+}
--- IPapi/routes/web.php
+++ IPapi/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IPController;
+
+Route::get('/region/{ip?}', [IPController::class, 'getIpRegion'])->name('ip.region');
 
 Route::get('/', function () {
     return view('welcome');
